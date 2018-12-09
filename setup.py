2cafcf7481d6c6e9453c9af767d6d9f279df8f8b
Matt Groth <mgroth49@gmail.com>
2018-12-09 16:53:33 -0500

    update

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='mjlib',  # How you named your package folder (MyLib)
     packages=find_packages(),  # Chose the same as name
-    version='0.1.85',  # Start with a small number and increase it with every change you make
+    version='0.1.86',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Matt\'s General Python Library',  # Give a short description about your library
     author='Matt Groth',  # Type in your name
     author_email='mgroth49@gmail.com',  # Type in your E-Mail
     url='https://github.com/mgroth0/matt_py_tools',  # Provide either the link to your github or to your website
-    download_url='https://github.com/mgroth0/matt_py_tools/releases/tag/0.1.85',  # I explain this later on
+    download_url='https://github.com/mgroth0/matt_py_tools/releases/tag/0.1.86',  # I explain this later on
     keywords=['matt', 'james', 'groth', 'library'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'matplotlib',
